e031ba952b7d143757ed5875dedd9f74ff5d8640
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-26 00:33:12 +0300

    Update BST.h

--- Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/BST.h
+++ Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/BST.h
@@ -1,25 +1,30 @@
 #ifndef BST_H_INCLUDED
 #define BST_H_INCLUDED
 
+// Node structure for the tree
+typedef struct Node {
+	int data;
+	struct Node* left;
+	struct Node* right;
+} Node;
+
 // Create Tree
-Node* createTreeNode(int data);
+Node* BST(int data);
 
 // Insert Function to insert elements in the tree
-Node insert(Node node, int element);
+Node* insert(Node* node, int element);
 
 // Delete Function to delete element in the tree
-Node* delete(Node node, int element);
+Node* delete(Node* node, int element);
 
 // search Function to search about specific element in the tree
-bool search(Node node, int element);
+bool search(Node* node, int element);
 
 // Traversals Function
 // printPreOrder Function to print preorder traversal (root -> left -> right)
-void printPreOrder(Node node);
+void printPreOrder(Node* node);
 
 // Free the tree to prevent memory leaks
 void freeTree(Node* node)
 
 #endif // BST_H_INCLUDED
-
-
