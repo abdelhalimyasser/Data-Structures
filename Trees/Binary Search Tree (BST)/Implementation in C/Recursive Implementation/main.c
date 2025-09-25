88797ca8cb418518b498d5fa0b7b407c07835768
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-26 00:37:47 +0300

    Update main.c

--- Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/main.c
+++ Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/main.c
@@ -5,10 +5,9 @@
 
 
 int main() {
-    Node* root = NULL;
+    Node* root = createBST(10);
 
     // Test inserting nodes: 10, 5, 15, 3, 7
-    root = insert(root, 10);
     root = insert(root, 5);
     root = insert(root, 15);
     root = insert(root, 3);
