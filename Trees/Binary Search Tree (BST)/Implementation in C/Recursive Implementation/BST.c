143a82050eeb1994f3ddb1fa564500a200bec42d
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-25 21:47:34 +0300

    Update BST.c

--- Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/BST.c
+++ Trees/Binary Search Tree (BST)/Implementation in C/Recursive Implementation/BST.c
@@ -3,3 +3,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+// Create Tree
+TreeNode* createTreeNode(int data)
+
+// Insert Function to insert elements in the tree
+Node insert(Node node, int element)
+
+// Delete Function to delete element in the tree
+TreeNode* delete(Node node, int element)
+
+// search Function to search about specific element in the tree
+bool search(Node node, int element)
+
+// Traversals Function
+// printPreOrder Function to print preorder traversal (root -> left -> right)
+void printPreOrder(Node node)
