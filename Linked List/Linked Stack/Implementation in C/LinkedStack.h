e5e822a883e74d334649a8ec2304fe28cb0ba1af
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-26 23:10:16 +0300

    Update LinkedStack.h

--- Linked List/Linked Stack/Implementation in C/LinkedStack.h
+++ Linked List/Linked Stack/Implementation in C/LinkedStack.h
@@ -1,6 +1,25 @@
 #ifndef LinkedStack_H_INCLUDED
 #define LinkedStack_H_INCLUDED
 
+// createLinkedStack function to create the LinkedStack
+LinkedStack* createLinkedStack();
 
+// push function to insert elements in the stack at the top
+void push(int element);
+
+// pop function to remove the last element
+int pop();
+
+// peek function to return the last element without removing it
+int peek();
+
+// size function to return the size of the stack
+int size();
+
+// isEmpty function to check if stack is full or not
+boolean isEmpty();
+
+// isFull function to check if stack is full or not
+boolean isFull();
 
 #endif // LinkedStack_H_INCLUDED
