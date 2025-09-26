493c677f06aa9820265a1fdc5771d56632f6fd3e
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-26 22:57:22 +0300

    Create Node.java

---
+++ Linked List/Linked Stack/Implementation in Java/Node.java
@@ -0,0 +1,12 @@
+import mypackage;
+
+// Define the node
+class Node {
+	int data;
+	Node next;
+	// Node constractor to define the node variables
+	Node(int data) {
+		this.data = data;
+		next = null;
+	}
+}
