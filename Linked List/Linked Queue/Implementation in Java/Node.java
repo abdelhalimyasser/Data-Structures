93190f9c9aa4a2797fc5984237af93eeff09686a
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-27 11:17:00 +0300

    Update Node.java

--- Linked List/Linked Queue/Implementation in Java/Node.java
+++ Linked List/Linked Queue/Implementation in Java/Node.java
@@ -1,2 +1,13 @@
 import mypackage;
 
+// Define the node
+class Node {
+    int data;
+    Node next;
+
+    // Node constractor to define the node variable
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
