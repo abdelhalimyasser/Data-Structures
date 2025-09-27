e5a387e7abae0b242644f95e8a9c97c9b3d4532a
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-27 17:34:24 +0300

    Update Main.java

--- Linked List/Linked Queue/Implementation in Java/Main.java
+++ Linked List/Linked Queue/Implementation in Java/Main.java
@@ -1,2 +1,37 @@
 import mypackage;
 
+public class Main
+{
+	public static void main(String[] args) {
+		LinkedQueue qeueue = new LinkedQueue();
+
+		qeueue.enqueue(10);
+		qeueue.enqueue(20);
+		qeueue.enqueue(30);
+		qeueue.print(); // Output: Queue: 10 20 30
+
+		System.out.println("Dequeued: " + qeueue.dequeue()); // Output: Dequeued: 10
+
+		qeueue.enqueue(40);
+		qeueue.enqueue(50);
+		qeueue.print(); // Output: Queue: 20 30 40
+
+		System.out.println("Front item: " + qeueue.peek()); // Output: Front item: 20
+		System.out.println("Size: " + qeueue.size()); // Output: Size: 3
+
+		// checking if Queue is empty
+		System.out.println("Is Queue empty: " + (qeueue.isEmpty() ? "Yes" : "No"));
+
+		// checking if Queue is full
+		System.out.println("Is Queue full: " + (qeueue.isFull() ? "Yes" : "No"));
+
+		qeueue.clear();
+		qeueue.print();
+    System.out.println("\nAfter Clear:");
+		// checking if Queue is empty
+		System.out.println("Is Queue empty: " + (qeueue.isEmpty() ? "Yes" : "No"));
+
+		// checking if Queue is full
+		System.out.println("Is Queue full: " + (qeueue.isFull() ? "Yes" : "No"));
+	}
+}
