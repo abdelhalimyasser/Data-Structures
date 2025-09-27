bbea3c0b868c9de1537aae1fc8d4d309579fceaf
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-27 11:11:03 +0300

    Update LinkedStack.c

--- Linked List/Linked Stack/Implementation in C/LinkedStack.c
+++ Linked List/Linked Stack/Implementation in C/LinkedStack.c
@@ -1,66 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "LinkedStack.h"
 
-// createLinkedStack function to create the LinkedStack
-Node* createLinkedStack(int data) {
+// Define the structure for a stack node
+typedef struct Node {
+	int data;
+	int size;
+	struct Node* next;
+} Node;
+
+
+// Function to create a new node
+Node* createNode(int data) {
 	Node* node = (Node*)malloc(sizeof(Node));
-	if (node == NULL) {
-		printf("Memory allocation failed!\n");
-		exit(1);
+	if (!node) {
+		printf("Memory allocation failed\n");
+		return NULL;
 	}
-	node->size = 0;
 	node->data = data;
+	node->size = -1;
+	node->next = NULL;
 	return node;
 }
 
 // push function to insert elements in the stack at the top
-void push(Node* node, int element) {
-	Node top = createLinkedStack(element);
+void push(Node** top, int* size, int element) {
+	Node* temp  = createNode(element);   // temp node to store the first element
 
-	size++;
+	temp->next = *top;      // make the assign the next to the temp
+	(*top) = temp;           //  assign the temp to the top
+	(*size)++;           // counter to increase the size of the Stack
 }
 
 // pop function to remove the last element
-int pop(Node* node) {
+int pop(Node** top, int* size) {
 	// condation to check if the stack is empty or not
-	if (top == null) {
+	if (*top == NULL) {
 		printf("Stack Underflow\n");
 		return -1;
 	}
-	size--;
 
-	return temp->data;
+	Node* temp  = *top;      // copy the top into temp node
+	int popped = temp->data;   // copy the the popped element from the the temp
+	*top = (*top)->next;    // assign the next of the top into top to make the pointer look at the next
+	(*size)--; // decrease the counter after removing the element
+    free(temp);     // free the temp to avoid memory leakege
+	
+	return popped;
 }
 
 // peek function to return the last element without removing it
-int peek(Node* node) {
+int peek(Node* top) {
 	// condation to check if the stack is empty or not
-	if (top == null)
+	if (top == NULL)
 		return -1;
 	return top->data;    // return the top element in the stack
 }
 
-// size function to return the size of the stack
-int size(Node* node) {
+// Print the stack elements
+void print(Node* top) {
 	// condation to check if the stack is empty or not
-	if (top == null)
-		return -1;
-	return top->size;    // return the size of the stack
+	if (top == NULL) {
+		printf("Stack is empty\n");
+		return;
+	}
+	int i = 0;
+	Node* temp = top;
+	while (temp != NULL) {
+		printf("[%d] -> %d\n", (i++), temp->data);
+		temp = temp->next;
+	}
 }
 
 // isEmpty function to check if stack is full or not
-bool isEmpty(Node* node) {
+bool isEmpty(Node* top) {
 	// condation to check if the stack is empty or not
-	if(top == null)
+	if(top == NULL)
 		return true;
 	return false;        // return false if the stack is full
 }
 
 // isFull function to check if stack is full or not
-bool isFull(Node* node) {
+bool isFull(Node* top) {
 	// condation to check if the stack is empty or not
-	if(top == null)
+	if(top == NULL)
 		return false;
 	return true;            // return true if the stack is full
+}
+
+// freeLinkedStack function to free out the stack to avoid memory leakage
+void freeLinkedStack(Node** top, int* size) {
+    while (*top != NULL) {
+        Node* temp = *top;
+        *top = temp->next;
+        free(temp);
+    }
+    *size = 0;
 }
