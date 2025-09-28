35b0cd6e7927fd6ad4f2443ffc431bb577ed697f
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-28 11:19:28 +0300

    Update LinkedQueue.h

--- Linked List/Linked Queue/Implementation in C/LinkedQueue.h
+++ Linked List/Linked Queue/Implementation in C/LinkedQueue.h
@@ -1,31 +1,43 @@
 #ifndef LinkedQueue_H_INCLUDED
 #define LinkedQueue_H_INCLUDED
 
-// Define the structure for a Queue node
+// Define node structure
+typedef struct Node {
+	int data;
+	struct Node* next;
+} Node;
 
+// Define queue structure
+typedef struct Queue {
+	Node* front;
+	Node* rear;
+} Queue;
 
 // Function to create a new node
 Node* createNode(int data);
 
+// Function to create an empty queue
+Queue* createQueue();
+
 // push function to insert elements in the queue at the rear
-void push(Node** , int* size, int element);
+void enqueue(Queue** queue, int* size, int element);
 
 // pop function to remove the front element
-int pop(Node** , int* size);
+int dequeue(Queue* queue, int* size);
 
 // peek function to return the front element without removing it
-int peek(Node* );
+int peek(Queue* queue);
 
 // Print the queue elements
-void print(Node* );
+void print(Queue* queue);
 
 // isEmpty function to check if queue is full or not
-bool isEmpty(Node* );
+bool isEmpty(Queue* queue);
 
 // isFull function to check if Queue is full or not
-bool isFull(Node* );
+bool isFull(Queue* queue);
 
 // freeLinkedQueue function to free out the queue to avoid memory leakage
-void freeLinkedQueue(Node** , int* size);
+void freeLinkedQueue(Queue** queue, int* size);
 
 #endif // LinkedQueue_H_INCLUDED
