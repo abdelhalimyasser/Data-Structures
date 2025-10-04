44d059d94e5cea85ba3669a1b755a4594758ccf3
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-10-04 18:15:30 +0300

    Update main.c

--- Binary Heap/Max Heap/Implementation in C/main.c
+++ Binary Heap/Max Heap/Implementation in C/main.c
@@ -3,3 +3,30 @@
 #define MAX_SIZE 100
 
 
+int main()
+{
+	int heap[MAX_SIZE];
+	int size = 0;
+
+	insert(heap, &size, 10);
+	insert(heap, &size, 5);
+	insert(heap, &size, 20);
+	insert(heap, &size, 2);
+	insert(heap, &size, 15);
+
+	printf("Max-Heap after insertions:\n");
+	printHeap(heap, size);
+
+	printf("Maximum: %d\n", getMax(heap, size));
+	printf("Minimum: %d\n", getMin(heap, size));
+
+	printf("Extracted Max: %d\n", extractMax(heap, &size));
+	printf("Max-Heap after extracting max:\n");
+	printHeap(heap, size);
+
+	printf("Extracted Min: %d\n", extractMin(heap, &size));
+	printf("Max-Heap after extracting min:\n");
+	printHeap(heap, size);
+
+	return 0;
+}
