a8970955b74a41b9e0b3c110c44a120d89ec5538
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-26 19:15:41 +0200

    Fix retrieval and memory management in linked list
    Updated retrieve function to properly delete nodes after retrieval and fixed memory management in destroy function.

--- Linked List/Linked List/Implementation of Linked List in C/linkedlist.c
+++ Linked List/Linked List/Implementation of Linked List in C/linkedlist.c
@@ -39,21 +39,35 @@ void insert(list* l, type element, int index)
 
 type retrieve(list* l, int index)
 {
-	if(index < 0 || index > l->size)
+    if(index < 0 || index > l->size)
+    {
+        printf("Invalid index: %d (current size: %d)\n", index, l->size);
+        return -999999;
+    }
+
+    type element;
+
+	if(index == 0)
 	{
-		printf("Invalid index: %d (current size: %d)\n", index, l->size);
-		return -999999;
+	    element = l->head->data;
+	    l->head = l->head->next; 
 	}
+	else
+    {
+        Node* temp = l->head;
+        for(int i = 0; i < index - 1; i++)
+            temp = temp->next;
 
-	Node* temp = l->head;
-
-	for(int i = 0; i < index; i++)
-		temp = temp->next;
-
-	l->size--;
-
-	return temp->data;
-
+        element = temp->next->data;
+        
+        Node* toDelete = temp->next;
+        temp->next = toDelete->next;
+        
+        free(toDelete);
+    }
+    
+    l->size--;
+    return element;
 }
 
 int isEmpty(list* l)
@@ -91,3 +105,4 @@ void printList(list* l)
 
 	printf("\n");
 }
+
